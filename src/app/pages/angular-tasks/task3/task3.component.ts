77736fb2a694f5061f96c75a13acd2a05cbbd850
Oleksandr-D <dolbnja9@gmail.com>
2022-11-09 09:02:59 +0200

    aded task2

--- src/app/pages/angular-tasks/task3/task3.component.ts
+++ src/app/pages/angular-tasks/task3/task3.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-task3',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task3.component.scss']
 })
 export class Task3Component implements OnInit {
+  public modalWindow = false;
+  public count = 1;
+  public inputTask = '';
+  public taskList = [{
+    inputTask: 'Here will be your task',
+    check: false,
+    buttonDelete: true
+  }];
+  public userTask = {
+    inputTask: '',
+    check: false,
+    buttonDelete: true
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  // modal window
+  showModal(): void {
+    this.modalWindow = !this.modalWindow;
+  };
+  // button Add
+  addTaskk(): void {
+    if (this.count < 6) {
+      if (this.inputTask.length > 1) {
+        this.userTask = {
+          inputTask: '',
+          check: false,
+          buttonDelete: true
+        };
+        this.userTask.inputTask = this.inputTask;
+        this.taskList.push(this.userTask)
+        this.inputTask = "";
+        ++this.count;
+      } else {
+        alert('Wrire some task')
+      }
+    }
   }
-
+  // Count of task from child component
+  countTask(data: number) {
+    this.count = data;
+  }
 }
