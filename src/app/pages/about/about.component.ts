3e0f9dcb343beacab69e6c06b90a3eadfb128f5a
Oleksandr-D <dolbnja9@gmail.com>
2023-07-27 12:53:05 +0300

    Added a dynamic background video, audio

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-
 export class AboutComponent implements OnInit {
 
   public url = 'https://drive.google.com/file/d/142BuwRYpHZuy9LUp-xFG0CKxeS83Uc9A/view?usp=drive_link';
@@ -14,4 +13,33 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  @HostListener('window:scroll', ['$event'])
+  
+  onScroll() {
+    this.handleScroll('.edu-scroll', '.lerning-scroll');
+    this.handleScroll('.ex-scroll', '.inv-scroll');
+  }
+
+  private handleScroll(h1Selector: string, pSelector: string) {
+    const h1Element = document.querySelector(h1Selector) as HTMLElement;
+    const pElement = document.querySelector(pSelector) as HTMLElement;
+
+    if (h1Element && pElement) {
+      const h1Offset = h1Element.getBoundingClientRect().top;
+      const pOffset = pElement.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (h1Offset < windowHeight - 100) {
+        h1Element.classList.add('scrolled-left');
+      } else {
+        h1Element.classList.remove('scrolled-left'); 
+      }
+
+      if (pOffset < windowHeight - 100) {
+        pElement.classList.add('scrolled');
+      } else {
+        pElement.classList.remove('scrolled');
+      }
+    }
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  isMobileScreen!: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
   }
 
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobileScreen = window.innerWidth < 1026;
+  }
+
+  shouldAutoplay() {
+    return !this.isMobileScreen;
+  }
 }
