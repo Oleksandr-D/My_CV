d7b955ec27c2dc084629dc194644fcea9e77b168
Oleksandr-D <dolbnja9@gmail.com>
2023-09-06 11:11:48 +0300

    added animation to work component

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-work',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.scss']
 })
 export class WorkComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -18,4 +18,23 @@ export class WorkComponent implements OnInit {
       behavior: 'smooth',
     });
   }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const scrollY = window.scrollY || window.pageYOffset;
+    const projectWrappers = document.querySelectorAll('.project-wraper');
+  
+    projectWrappers.forEach((wrapper: Element) => {
+      if (scrollY >= 50 && wrapper.classList.contains('one')) {
+        wrapper.classList.add('scroll-active');
+      } else if (scrollY >= 160 && wrapper.classList.contains('two')) {
+        wrapper.classList.add('scroll-active');
+      } else if (scrollY >= 450 && wrapper.classList.contains('three')) {
+        wrapper.classList.add('scroll-active');
+      } else {
+        wrapper.classList.remove('scroll-active');
+      }
+    });
+  }
+  
 }
